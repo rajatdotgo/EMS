54c91b5e144b359c3e7dbe1099c0462c6a96cf22
Rajat Goyal <rajatgoyal044@gmail.com>
2019-10-30 16:57:58 +0530

    Docker file added

--- src/main/java/com/rajat/demoemp1/controller/ControllerClass.java
+++ src/main/java/com/rajat/demoemp1/controller/ControllerClass.java
@@ -49,62 +49,62 @@ public class ControllerClass {
 
     }
 
-//
-//    @PostMapping(path = "/rest/employees")
-//    @ApiOperation(value="Adds a new employee in the organisation")
-//    public ResponseEntity<String> saveData(@RequestBody PostRequest employee)
-//    {
-//        return empService.addEmployee(employee);
-//    }
-//
-//    @PutMapping("/rest/employees/{empId}")
-//    @ApiOperation(value="Updates a particular employee by Id ")
-//    public ResponseEntity putData(@ApiParam(value = "Employee unique id whose details you need to update",required = true)@PathVariable("empId") int empId, @RequestBody PutRequest emp) {
-//
-//
-//        if(empId<0)
-//        {
-//            return new ResponseEntity(message.getMessage("INVALID_ID"),HttpStatus.BAD_REQUEST);
-//        }
-//        if (!empValidate.empExist(empId)) {
-//            return new ResponseEntity(message.getMessage("NO_RECORD_FOUND"), HttpStatus.BAD_REQUEST);
-//        }
-//        if((emp.getName()==null||emp.getName().equals(""))&&(emp.getManagerId()==null)&&(emp.getJobTitle()==null||emp.getJobTitle().equals("")))
-//        {
-//            return new ResponseEntity<>(message.getMessage("INSUFFICIENT_DATA"),HttpStatus.BAD_REQUEST);
-//        }
-//        if (empRepo.findByEmpId(empId).designation.getDesId() == 1 &&(!emp.getJobTitle().equals("Director"))) {
-//            return new ResponseEntity("You can not alter the Director", HttpStatus.BAD_REQUEST);
-//        }
-//        if(emp.getName().matches(".*\\d.*"))
-//        {
-//            return new ResponseEntity(message.getMessage("INVALID_NAME"),HttpStatus.BAD_REQUEST);
-//        }
-//
-//        ResponseEntity re= null;
-//
-//
-//
-//        // when replace is true
-//        if (emp.isReplace()) {
-//           re  = empService.replaceEmployee(empId,emp);
-//        }
-//        // when replace is false
-//        else {
-//            re  = empService.employeeUpdate(empId, emp);
-//        }
-//        return re;
-//    }
-//    @DeleteMapping("/rest/employees/{employeeId}")
-//    @ApiOperation(value="Delete an employee by id otherwise suitable response")
-//    public ResponseEntity deleteEmployee(@ApiParam(value = "Employee unique id whom you want to delete",required = true)@PathVariable("employeeId") int employeeId)
-//    {
-//        if(employeeId<0)
-//        {
-//            return new ResponseEntity(message.getMessage("INVALID_ID"),HttpStatus.BAD_REQUEST);
-//        }
-//       return  empService.deleteEmployee(employeeId);
-//    }
+
+    @PostMapping(path = "/rest/employees")
+    @ApiOperation(value="Adds a new employee in the organisation")
+    public ResponseEntity<String> saveData(@RequestBody PostRequest employee)
+    {
+        return empService.addEmployee(employee);
+    }
+
+    @PutMapping("/rest/employees/{empId}")
+    @ApiOperation(value="Updates a particular employee by Id ")
+    public ResponseEntity putData(@ApiParam(value = "Employee unique id whose details you need to update",example = "1",required = true)@PathVariable("empId") int empId, @RequestBody PutRequest emp) {
+
+
+        if(empId<0)
+        {
+            return new ResponseEntity(message.getMessage("INVALID_ID"),HttpStatus.BAD_REQUEST);
+        }
+        if (!empValidate.empExist(empId)) {
+            return new ResponseEntity(message.getMessage("NO_RECORD_FOUND"), HttpStatus.BAD_REQUEST);
+        }
+        if((emp.getName()==null||emp.getName().equals(""))&&(emp.getManagerId()==null)&&(emp.getJobTitle()==null||emp.getJobTitle().equals("")))
+        {
+            return new ResponseEntity<>(message.getMessage("INSUFFICIENT_DATA"),HttpStatus.BAD_REQUEST);
+        }
+        if (empRepo.findByEmpId(empId).designation.getDesId() == 1 &&(!emp.getJobTitle().equals("Director"))) {
+            return new ResponseEntity("You can not alter the Director", HttpStatus.BAD_REQUEST);
+        }
+        if(emp.getName().matches(".*\\d.*"))
+        {
+            return new ResponseEntity(message.getMessage("INVALID_NAME"),HttpStatus.BAD_REQUEST);
+        }
+
+        ResponseEntity re= null;
+
+
+
+        // when replace is true
+        if (emp.isReplace()) {
+           re  = empService.replaceEmployee(empId,emp);
+        }
+        // when replace is false
+        else {
+            re  = empService.employeeUpdate(empId, emp);
+        }
+        return re;
+    }
+    @DeleteMapping("/rest/employees/{employeeId}")
+    @ApiOperation(value="Delete an employee by id otherwise suitable response")
+    public ResponseEntity deleteEmployee(@ApiParam(value = "Employee unique id whom you want to delete",example = "1",required = true)@PathVariable("employeeId") int employeeId)
+    {
+        if(employeeId<0)
+        {
+            return new ResponseEntity(message.getMessage("INVALID_ID"),HttpStatus.BAD_REQUEST);
+        }
+       return  empService.deleteEmployee(employeeId);
+    }
 
 }
 
